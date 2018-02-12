b61b56206b1cfe5d773a28a3b31ee2b8a89cc15e
tksmrkm <nis.murakami@gmail.com>
2018-02-12 15:01:33 +0900

    set basename

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import configureStore, { history } from './utils/configureStore'
+import { BrowserRouter } from 'react-router-dom'
 import {
     ConnectedRouter,
     // push
@@ -10,13 +11,17 @@ import App from './components/App'
 
 const store = configureStore()
 
+const basename = process.env.NODE_ENV === 'development' ? '/' : '/game-timer/'
+
 // Now you can dispatch navigation actions from anywhere!
 // store.dispatch(push('/foo'))
 
 render(
     <Provider store={store}>
         <ConnectedRouter history={history}>
-            <App />
+            <BrowserRouter basename={basename}>
+                <App />
+            </BrowserRouter>
         </ConnectedRouter>
     </Provider>
     , document.getElementById('app')
